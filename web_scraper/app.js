bcfc949a69a26567aa888edca607131bd9bdd7ac
Dennis Li (Coursera) <dli@coursera.org>
2015-04-17 16:32:51 -0400

    Working HN scraper

--- web_scraper/app.js
+++ web_scraper/app.js
@@ -4,10 +4,26 @@ var cheerio = require('cheerio');
 var url = 'https://news.ycombinator.com/';
 
 request(url, function(error, response, body) {
-  var data = {};
-
   var $ = cheerio.load(body);
-  var rows = $('table table tr');
+  var titleContainers = $('td.title').has('.sitebit.comhead');
 
-  console.log(rows.text())
-})
+  var data = [];
+
+  // equivalent to a for loop
+  titleContainers.each(function(index, titleContainer) {
+    var titleLink = $(titleContainer).find('a');
+
+    var comments = $(titleContainer).parent().next().find('.subtext');
+
+    data[index] = {
+      title: titleLink.text(),
+      link: titleLink.attr('href'),
+      source: $(titleContainer).find('.sitebit.comhead').text(),
+      score: comments.find('.score').text(),
+      submitter: $(comments.find('a').get(0)).text(),
+      numberComments: $(comments.find('a').get(1)).text(),
+    };
+  });
+
+  console.log(data);
+});
