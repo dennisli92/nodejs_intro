fa6f78597db67db27fddddce576546fec0a8ed62
Dennis Li (Coursera) <dli@coursera.org>
2015-04-17 14:58:16 -0400

    Simple http server

--- simple_server/app.js
+++ simple_server/app.js
@@ -1,8 +1,14 @@
+// We need to use the http library. So we make it accessible to app.js by requiring it through the variable http
 var http = require('http');
 
-http.createServer(function(request, response) {
+// Returns a server object 
+// Pass in an anonymous function that handles the request/response
+var server = http.createServer(function(request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World!\n')
-}).listen(8080);
+  response.end('Hello World!\n');
+});
+
+// make server listen to port 8080
+server.listen(8080);
 
 console.log('Server running on port 8080.');
